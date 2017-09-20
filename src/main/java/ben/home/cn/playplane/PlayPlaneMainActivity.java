ec3c57c70cd162290efd747838c1b16980a6bce9
four-pigs <rx9702@163.com>
2017-09-20 16:52:30 +0800

    Update the AppCompatActivity and Activity difference set full screen code.

--- src/main/java/ben/home/cn/playplane/PlayPlaneMainActivity.java
+++ src/main/java/ben/home/cn/playplane/PlayPlaneMainActivity.java
@@ -1,13 +1,20 @@
 package ben.home.cn.playplane;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.app.Activity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class PlayPlaneMainActivity extends AppCompatActivity {
-
+//public class PlayPlaneMainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);  // Full screen mode, remove title
+        getSupportActionBar().hide();       /* AppCompatActivity use this to become full screen */
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);    // Full screen, no status bar
         setContentView(R.layout.activity_play_plane_main);
     }
 }
