e14a57b7143398a4a4c330f2a9a32581d5353898
four-pigs <rx9702@163.com>
2017-09-20 16:11:54 +0800

    Original version with auto make source only, target set git environment.

---
+++ src/main/java/ben/home/cn/playplane/PlayPlaneMainActivity.java
@@ -0,0 +1,13 @@
+package ben.home.cn.playplane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayPlaneMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_plane_main);
+    }
+}
